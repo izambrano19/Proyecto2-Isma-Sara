8c4befa176f11e6f28e007e1436f0f93b2bc8981
izambrano19 <izambrano19@ilg.cat>
2023-01-20 16:13:27 +0100

    20-01-2023

--- index.php
+++ index.php
@@ -19,18 +19,40 @@
         
         
         <div class="containerprimero">
-            <img src="img/noticias2/c1.jpg" alt="Imagen-1">
+            <img src="img/noticias/biomedicina1.jpg" alt="Imagen-1">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra id lacus id aliquam. Nunc enim massa, cursus dignissim diam a, pharetra dictum augue. Mauris non lacus rhoncus, bibendum odio ultrices, sodales diam. In ornare, metus eu suscipit venenatis, ligula tellus euismod dui, auctor iaculis dolor ante vitae tortor. Fusce pellentesque ac arcu vitae bibendum. Aliquam nec ex vitae erat vehicula facilisis. Proin dapibus nisi ut nulla tempor pretium. Aliquam eu magna eget metus commodo eleifend sed at enim. Morbi ornare posuere purus</p>
 
         </div>
         <div class="containersegundo">
-        <img src="img/noticias2/c2.jpg" alt="Imagen-2">
+        <img src="img/noticias/biomedicina2.jpg" alt="Imagen-2">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra id lacus id aliquam. Nunc enim massa, cursus dignissim diam a, pharetra dictum augue. Mauris non lacus rhoncus, bibendum odio ultrices, sodales diam. In ornare, metus eu suscipit venenatis, ligula tellus euismod dui, auctor iaculis dolor ante vitae tortor. Fusce pellentesque ac arcu vitae bibendum. Aliquam nec ex vitae erat vehicula facilisis. Proin dapibus nisi ut nulla tempor pretium. Aliquam eu magna eget metus commodo eleifend sed at enim. Morbi ornare posuere purus</p>
 
         </div>
 
     </div>
-    <img src="img/noticias2/c3.jpeg" alt="Imagen-3">
+    
+
+    <div class="tituloNoticias">
+        <h2>LABORATORIO</h2>
+    </div>
+    <div class="containerComunes1" style="background-color: #CBEDD5;">
+
+        
+        
+    <div class="containerprimero">
+        <img src="img/noticias2/c3.jpeg" alt="Imagen-1">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra id lacus id aliquam. Nunc enim massa, cursus dignissim diam a, pharetra dictum augue. Mauris non lacus rhoncus, bibendum odio ultrices, sodales diam. In ornare, metus eu suscipit venenatis, ligula tellus euismod dui, auctor iaculis dolor ante vitae tortor. Fusce pellentesque ac arcu vitae bibendum. Aliquam nec ex vitae erat vehicula facilisis. Proin dapibus nisi ut nulla tempor pretium. Aliquam eu magna eget metus commodo eleifend sed at enim. Morbi ornare posuere purus</p>
+
+    </div>
+    <div class="containersegundo">
+        <img src="img/noticias2/c2.jpg" alt="Imagen-2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra id lacus id aliquam. Nunc enim massa, cursus dignissim diam a, pharetra dictum augue. Mauris non lacus rhoncus, bibendum odio ultrices, sodales diam. In ornare, metus eu suscipit venenatis, ligula tellus euismod dui, auctor iaculis dolor ante vitae tortor. Fusce pellentesque ac arcu vitae bibendum. Aliquam nec ex vitae erat vehicula facilisis. Proin dapibus nisi ut nulla tempor pretium. Aliquam eu magna eget metus commodo eleifend sed at enim. Morbi ornare posuere purus</p>
+
+    </div>
+
+</div>
+
+
 
     <div class="containerComunes2">
 
