93afc1698504f179121ec68025d86d1449f8f905
izambrano19 <izambrano19@ilg.cat>
2023-02-02 17:33:08 +0100

    PAGINA PROTEINAS

--- proteins.php
+++ proteins.php
@@ -10,57 +10,64 @@
 
 
     <div>
-        <h2>PROTEINS</h2>
+        <h2 style="text-align: center">PROTEINS</h2>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
+<br>
 
 <!-- BARRA DE NAVEGACION -->
 
 
-<div class="containerProteinas">
-    <input type="button" value="NEW ITEM">
-    <input type="button" value="MY ITEM">
+<div class="field" id="searchform">
+  <input type="text" id="searchterm" placeholder="Proteina?" />
+  <button type="button" id="search"><img class="iconSearch" src="https://img.icons8.com/material-outlined/256/search.png"></button>
+</div>
 
+<script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+
+
+
+
+
+<div class="containerProteinas">
+    <div class="containerBotonItems">
+        <input class="botonItems" type="button" value="NEW ITEM">
+        <input class="botonItems" type="button" value="MY ITEM">
+    </div>
 
 <div class="containerColProteinas"> 
 		<div class="item"> 
+            <img class="icon_Eliminar" src="https://img.icons8.com/ios-filled/256/delete.png"/>
             <img class="icon_Editar" src="https://img.icons8.com/pastel-glyph/64/null/edit--v1.png"/>
-            <div class="parte_Superior">X</div>
-
             <img src="img/proteinas/proteina-1.png">
 		</div> 
 		<div class="item"> 
-            <img src="img/proteinas/proteina-2.jpg">
+        <img class="icon_Eliminar" src="https://img.icons8.com/ios-filled/256/delete.png"/>
+            <img class="icon_Editar" src="https://img.icons8.com/pastel-glyph/64/null/edit--v1.png"/>    
+        <img src="img/proteinas/proteina-2.jpg">
 		</div> 
         <div class="item"> 
+        <img class="icon_Eliminar" src="https://img.icons8.com/ios-filled/256/delete.png"/>
+            <img class="icon_Editar" src="https://img.icons8.com/pastel-glyph/64/null/edit--v1.png"/>
             <img src="img/proteinas/proteina-3.jpg">
 		</div> 
 		<div class="item"> 
+        <img class="icon_Eliminar" src="https://img.icons8.com/ios-filled/256/delete.png"/>
+            <img class="icon_Editar" src="https://img.icons8.com/pastel-glyph/64/null/edit--v1.png"/>
             <img src="img/proteinas/proteina-1.png">
 		</div> 
         <div class="item"> 
+        <img class="icon_Eliminar" src="https://img.icons8.com/ios-filled/256/delete.png"/>
+            <img class="icon_Editar" src="https://img.icons8.com/pastel-glyph/64/null/edit--v1.png"/>
             <img src="img/proteinas/proteina-2.jpg">
 		</div> 
 		<div class="item"> 
+        <img class="icon_Eliminar" src="https://img.icons8.com/ios-filled/256/delete.png"/>
+            <img class="icon_Editar" src="https://img.icons8.com/pastel-glyph/64/null/edit--v1.png"/>
             <img src="img/proteinas/proteina-3.jpg">
 		</div> 
 	</div> 
 
-    <br>
-    <div class="containerColProteinas">
-        <div style="background-color: red; width: 100px; height: 50px"></div>
-        <div class="item"> 
-                <div class="parte_Superior"></div>
-                <img src="img/proteinas/proteina-1.png">
-        </div>
-        <div class="item"> 
-            <div class="parte_Superior"></div>
-            <img src="img/proteinas/proteina-1.png">
-		</div>
-    </div>
+
 
 
 </div>
