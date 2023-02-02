51d3be4311f28f71d72b04210cfa601ef295bbc5
izambrano19 <izambrano19@ilg.cat>
2023-02-02 18:35:15 +0100

    PAGINA FARMACOS

--- proteins.php
+++ proteins.php
@@ -22,19 +22,17 @@
   <button type="button" id="search"><img class="iconSearch" src="https://img.icons8.com/material-outlined/256/search.png"></button>
 </div>
 
-<script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+<!--<script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>-->
 
 
-
-
-
-<div class="containerProteinas">
-    <div class="containerBotonItems">
+<div class="containerBotonItems">
         <input class="botonItems" type="button" value="NEW ITEM">
         <input class="botonItems" type="button" value="MY ITEM">
     </div>
 
-<div class="containerColProteinas"> 
+<div class="containerProteinas">
+    
+    <div class="containerColProteinas"> 
 		<div class="item"> 
             <img class="icon_Eliminar" src="https://img.icons8.com/ios-filled/256/delete.png"/>
             <img class="icon_Editar" src="https://img.icons8.com/pastel-glyph/64/null/edit--v1.png"/>
@@ -66,8 +64,6 @@
             <img src="img/proteinas/proteina-3.jpg">
 		</div> 
 	</div> 
-
-
 
 
 </div>
