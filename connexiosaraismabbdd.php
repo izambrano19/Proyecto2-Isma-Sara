ffbc4259d9d89110184fae940d4ef88e9064aba4
Sara Zambrano <szambrano21@ilg.cat>
2023-02-09 18:01:12 +0100

    mm

--- connexiosaraismabbdd.php
+++ connexiosaraismabbdd.php
@@ -4,7 +4,7 @@ $database = "saraismabbdd";
 $user = "IsmaelSara";
 $password = "12345678";
 $conexion = mysqli_connect($host, $user, $password,$database);
-
+define('NUM_ITEMS_BY_PAGE', 6);
 if (!$conexion) die("No ha podido realizarse la conexión".mysqli_connect_error());
 else 
 ?>
