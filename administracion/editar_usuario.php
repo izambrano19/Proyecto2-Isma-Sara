21c256e18382884f826eae168cc5ef335e53d4f8
izambrano19 <izambrano19@ilg.cat>
2023-03-01 20:50:44 +0100

    cambios

--- administracion/editar_usuario.php
+++ administracion/editar_usuario.php
@@ -4,40 +4,37 @@ include_once('connexiosaraismabbdd.php');
 
 if(!empty($_POST)){
     $alert="";
-    if(empty($_POST["nombre"]) || empty($_POST["dni"]) || empty($_POST["tipo"])){
-
+    if(empty($_POST["nombre"]) || empty($_POST["dni_usuario"]) || empty($_POST["tipo"])){
         $alert="<p class='msg_error'>Todos los campos son obligatorios</p>";
     }else{
 
         $nombre = $_POST["nombre"];
         $dni = $_POST["dni"];
-        $dni_user = $_POST["dni_usuario"];
+        $dni_usuario = $_POST["dni_usuario"];
         $pass = md5($_POST["password"]);
         $tipo = $_POST["tipo"];
 
-        $query = mysqli_query($conexion,"SELECT * FROM tusuario WHERE NombreUsuario = '$nombre' AND DNI != '$dni'
+        $query = mysqli_query($conexion,"SELECT * FROM tusuario WHERE DNI = '$dni'
+
         ");
         $resultado = mysqli_fetch_assoc($query);
 
         if($resultado > 0){
             $alert="<p class='msg_error'>El usuario ya existe</p>";
         }else{
-
             if(empty($_POST['password'])){
                 $sql_update = mysqli_query($conexion, "UPDATE tusuario
                 SET NombreUsuario = '$nombre', Tipo = '$tipo', DNI = '$dni'
-                WHERE DNI = '$dni_user'");
+                WHERE DNI = '$dni_usuario'");
             }else{
                 $sql_update = mysqli_query($conexion, "UPDATE tusuario
                 SET NombreUsuario = '$nombre', Tipo = '$tipo', DNI = '$dni', Password = '$pass'
-                WHERE DNI = '$dni_user'");
+                WHERE DNI = '$dni_usuario'");
             }
-
             if($sql_update){
-                echo '<script> alert("usuario_creado")</script>';
                 $alert="<p class='msg_correcto'>El usuario ha sido actualizado correctamente</p>";
             }else{
-                $alert="<p class='msg_error'>Error al actualizazr el usuario</p>";
+                $alert="<p class='msg_error'>Error al actualizar el usuario</p>";
             }
         }
     }
@@ -47,10 +44,11 @@ if(!empty($_POST)){
 if(empty($_GET['DNI'])){
     header("location: listado_usuarios.php");
 }
-
+/* aqui revisarrrrrrrrrrrrrrr */
+$dni = $_GET['DNI'];
 $dni_usuario = $_GET['DNI'];
 
-$sql = mysqli_query($conexion,"SELECT DNI, Tipo, NombreUsuario, Password FROM tusuario WHERE DNI = '$dni_usuario'");
+$sql = mysqli_query($conexion,"SELECT DNI, Tipo, NombreUsuario, Password FROM tusuario WHERE DNI = '$dni'");
 
 $resultado_sql = mysqli_num_rows($sql);
 
@@ -60,7 +58,6 @@ if($resultado_sql == 0){
     $option = '';
     while($row = mysqli_fetch_assoc($sql)){
 
-        $dni_usuario = $row['DNI'];
         $nombre = $row['NombreUsuario'];
         $tipo = $row['Tipo'];
 
@@ -97,15 +94,13 @@ if($resultado_sql == 0){
 <div class="alert"> <?php echo isset($alert) ? $alert : ''; ?> </div>
 
 <form action="" method="post">
-    <input type="hidden" name="dni_usuario" value="<?php echo "$dni_usuario";?>">
 
+<input type="hidden" name="dni_usuario" value="<?php echo $dni_usuario; ?>">
     <label for="nombre">Nombre</label>
     <input type="text" name="nombre" id="nombre" placeholder="Nombre" value="<?php echo $nombre;?> ">
 
-
     <label for="dni">DNI</label>
-    <input type="text" name="dni" id="dni" placeholder="DNI" minlength="9" maxlength="9" value="<?php echo $dni_usuario;?>">
-
+    <input type="text" name="dni" id="dni" placeholder="DNI" minlength="9" maxlength="9" value="<?php echo $dni;?>">
 
     <label for="password">Contraseña</label>
     <input type="password" name="password" id="password" placeholder="Contraseña">
