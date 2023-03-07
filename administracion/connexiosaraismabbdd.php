802888de484f391e61fc8f4acf145862f9b53522
izambrano19 <izambrano19@ilg.cat>
2023-03-07 02:24:22 +0100

    avancesss

--- administracion/connexiosaraismabbdd.php
+++ administracion/connexiosaraismabbdd.php
@@ -1,8 +1,21 @@
 <?php
+/*
 $host = "projectedawilg3.cat";
 $database = "zb5b2zsl_saraismabbdd";
 $user = "zb5b2zsl_IsmaelSara";
 $password = "proyecto_daw";
+$conexion = mysqli_connect($host, $user, $password,$database);
+define('NUM_ITEMS_BY_PAGE', 6);
+if (!$conexion) die("No ha podido realizarse la conexión".mysqli_connect_error());
+else 
+*/
+
+?>
+<?php
+$host = "localhost";
+$database = "saraismabbdd";
+$user = "IsmaelSara";
+$password = "12345678";
 $conexion = mysqli_connect($host, $user, $password,$database);
 define('NUM_ITEMS_BY_PAGE', 6);
 if (!$conexion) die("No ha podido realizarse la conexión".mysqli_connect_error());
